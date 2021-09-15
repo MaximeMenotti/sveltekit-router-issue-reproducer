f7581d2b141e9d6e23bf6684aca48bed4c5cc4e2
Maxime Menotti <maxime.menotti@decathlon.com>
2021-09-15 17:51:08 +0200

    :sparkles: Add svelte consumer

---
+++ svelte-content-consumer/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import {onMount} from "svelte";
+
+    let content = ""
+
+    onMount(async () => {
+      const res = await fetch(`http://localhost:3000/api/render`)
+
+      content = await res.text()
+    });
+</script>
+
+<svelte:head>
+    <link rel="stylesheet" href="http://localhost:3000/sphere-content.css" />
+    <script src="http://localhost:3000/sphere-content.js" defer />
+</svelte:head>
+
+<h1>Hello</h1>
+<div id="sphere-content"> {@html content} </div>
